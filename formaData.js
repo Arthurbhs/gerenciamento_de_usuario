cd42db631f35457e2362040c6387ea1d1731acb3
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-09-23 15:00:52 +0000

    formatacao  de data e hora

---
+++ formaData.js
@@ -0,0 +1,9 @@
+// staico é o metodo que não se cria uma estancia
+class util{
+static dateformat(date){
+
+return date.getDate()+'/'+(date.getmonth()+1)=date.getFullyear()+' '+date.getHours()+':'+date.getMinutes();
+
+}
+
+}
